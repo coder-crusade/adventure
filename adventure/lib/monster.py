c20d3d92ab721b5eea1eed0e6c21a6a4c7e7d682
Mark Bell <skrambelled@gmail.com>
2020-12-09 13:24:59 -0800

    ported player commands from the brain to the player object

--- adventure/lib/monster.py
+++ adventure/lib/monster.py
@@ -1,5 +1,20 @@
-from adventure.lib.living import Living 
+from adventure.lib.living import Living
+import random
 
 
 class Monster(Living):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.angry = []
+
+    def choose_action():
+        if len(self.angry):
+            choose_one_to_hit = []
+            for obj in self.environment.inventory:
+                if obj in self.angry:
+                    choose_one_to_hit.append(obj)
+            if len(choose_one_to_hit):
+                (random.choice(choose_one_to_hit)).hit(self.attack_value)
+
+        
+            
--- adventure/lib/player.py
+++ adventure/lib/player.py
@@ -2,4 +2,100 @@ from adventure.lib.living import Living
 
 
 class Player(Living):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.actions = {
+            # cardinal directions are always an available action,
+            # so if the room does not have those exits, lets handle them
+            'north'  : self.do_not_move_player,
+            'south'  : self.do_not_move_player,
+            'east'   : self.do_not_move_player,
+            'west'   : self.do_not_move_player,
+
+            # strike
+            'strike' : self.do_strike,
+        }
+
+
+    def action(self, verb, args=None):
+        """
+        action handler for the player object
+
+        return values:
+        False - repesents an action that was not resolved
+        True  - represets an action that was resolved
+        """
+
+
+        # cardinal directions are special commands, and are allowed to be shortcutted:
+        # expand short directions into longer directions
+        if verb == 'n':
+            verb = 'north'
+        elif verb == 's':
+            verb = 'south'
+        elif verb == 'e':
+            verb = 'east'
+        elif verb == 'w':
+            verb = 'west'
+
+        # is the action part of the environments exit list?
+        if verb in self.environment.exits.keys():
+            return self.do_move_player(verb, args)
+
+        # is the action is in the players action list?
+        if verb in self.actions:
+            return self.actions[verb](verb, args)
+
+        # is the action in some obejct in the inventory of the player?
+        for obj in self.inventory:
+            if verb in obj.actions:
+                if not obj[actions](verb, args):
+                    continue
+
+    def do_move_player(self, verb, args):
+        """
+        action to move a player in a cardinal direction
+        """
+
+        print(f'You walk {verb}')
+        self.move(self.environment.exits[verb])
+        return True
+
+
+    def do_not_move_player(self, verb, arg):
+        print(f'You bump into a wall as you try to walk {verb}.')
+        return True
+
+
+    def do_strike(self, verb, args):
+        for thing in player.environment.inventory:
+
+            if thing.name.lower() != args:
+                continue 
+
+            if not isinstance(thing, Living):
+                print(f'You cannot strike ')
+                return True
+
+            if not thing.is_alive:
+                return True
+
+            print(f"You hit the {thing.name} for {player.attack_value} damage!")
+            damage = thing.hit(player.attack_value)
+            if not self in thing.angry:
+                print(f'{thing.name} snarls at you!')
+                thing.angry.append(self)
+            
+            return True
+
+
+            if thing.health > 0:
+                damage = player.hit(thing.attack_value)
+                print(f"The {thing.name} hits you for {damage} damage!")
+
+            break 
+
+
+
+
+    
