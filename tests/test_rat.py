a38064725154ca3e356994650762ab097c38763b
Hexx King <hexxking13@gmail.com>
2020-12-10 14:00:11 -0800

    all 'Happy Path' tests written for rat.py and passing

--- tests/test_rat.py
+++ tests/test_rat.py
@@ -1,5 +1,6 @@
 from adventure.monsters.rat import Rat
 from adventure.lib.player import Player
+from adventure.lib.room import Room
 
 #=====TEST CONNECTION======
 def test_import_Rat():
@@ -24,11 +25,26 @@ def test_respond_to_hit(capsys):
     expected = "- - - Keep striking it! - - -\n"
     assert expected == actual
 
-# def test_do_search_corpse(capsys):
-#     rat = Rat()
-#     player = Player()
-#     rat.do_search_corpse('search', rat, player)
-#     captured = capsys.readouterr()
-#     actual = captured.out
-#     expected = True
-#     assert expected == actual
+def test_do_search_corpse(capsys):
+    rat = Rat()
+    room = Room()
+    player = Player()
+    rat.move(room)
+    player.move(room)
+    actual_1 = rat.do_search_corpse('search', rat, player)
+    expected_1 = True
+    captured = capsys.readouterr()
+    actual_2 = captured.out
+    expected_2 = "As you search the Rat you hear a Key hit the floor! You should 'Collect' it!\n"
+    assert expected_1 == actual_1
+    assert expected_2 == actual_2
+    
+
+def test_hit(capsys):
+    rat = Rat()
+    rat.hit(1)
+    rat.hit(1)
+    captured = capsys.readouterr()
+    actual = captured.out
+    expected = "Rat has died!\nPerhaps you can 'Search Rat'\n"
+    assert expected == actual
